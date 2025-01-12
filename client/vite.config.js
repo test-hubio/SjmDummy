6089e2f1629931a02359bb74b2edb2e563b1b2c3
marvellous-is-cool <adebayoinioluwamarvellous1@gmail.com>
2025-01-12 19:48:01 +0100

    SJMINFIVERR - client config to /app

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/app/', // Set the base path to match your production environment
   plugins: [react()],
-})
+});
