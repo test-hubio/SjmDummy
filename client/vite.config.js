23f8befab06d9ff54f4d93ab0acdc417c0507351
marvellous-is-cool <adebayoinioluwamarvellous1@gmail.com>
2025-01-13 10:23:29 +0100

    SJMINFIVERR - client configA

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// Log the VITE_API_URL environment variable to the console
+console.log('VITE_API_URL:', process.env.VITE_API_URL);
+
 export default defineConfig({
   base: '/',
   plugins: [react()],
