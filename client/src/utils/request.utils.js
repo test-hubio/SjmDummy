da8ce1948b09ce481a4f096e1f7fb6ff36f16fb6
marvellous-is-cool <adebayoinioluwamarvellous1@gmail.com>
2025-01-13 10:05:31 +0100

    SJMINFIVERR - client config2

--- client/src/utils/request.utils.js
+++ client/src/utils/request.utils.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Determine the environment (production or development)
 const API_URL = import.meta.env.MODE === 'production' 
   ? import.meta.env.VITE_API_URL // Use the production API URL if in production
-  : "http://localhost:7800/api";  // Use localhost if in development
+  : "https://server-production-30d4.up.railway.app//api";  // Use localhost if in development
 
 
 console.log('API URL from env:', import.meta.env.VITE_API_URL);
