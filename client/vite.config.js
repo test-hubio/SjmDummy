03e87fc4a35cb5399e031c631f0d782f141be3a0
marvellous-is-cool <adebayoinioluwamarvellous1@gmail.com>
2025-01-13 07:05:40 +0100

    SJMINFIVERR - client config to /app5

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/app/', // Set the base path to match your production environment
+  base: '/app/', // Adjust the base path if necessary
   plugins: [react()],
+  server: {
+    port: import.meta.env.CLIENT_PORT || 5173, // Use the provided port from the environment
+  },
 });
