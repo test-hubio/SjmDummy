85e56f2901e3a43136fd32f2e09c7b4e865e64c0
marvellous-is-cool <adebayoinioluwamarvellous1@gmail.com>
2025-01-13 10:59:09 +0100

    SJMINFIVERR - client config

--- client/src/utils/request.utils.js
+++ client/src/utils/request.utils.js
@@ -1,13 +1,9 @@
 import axios from "axios";
 
 // Determine the environment (production or development)
-const API_URL = import.meta.env.MODE === 'production' 
-  ? import.meta.env.VITE_API_URL // Use the production API URL if in production
-  : "https://server-production-30d4.up.railway.app//api";  // Use localhost if in development
+const API_URL = process.env.VITE_API_URL || 'http://localhost:7800/api'  // Use localhost if in development
 
-
-console.log('API URL from env:', import.meta.env.VITE_API_URL);
-
+console.log('API URL from env:', process.env.VITE_API_URL);
 
 const request = axios.create({
   baseURL: API_URL,
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// Log the VITE_API_URL environment variable to the console
-console.log('VITE_API_URL:', process.env.VITE_API_URL);
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode`
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  // Log the loaded environment variables
+  console.log('Mode:', mode)
+  console.log('VITE_API_URL:', env.VITE_API_URL)
 
-export default defineConfig({
-  base: '/',
-  plugins: [react()],
-});
+  return {
+    base: '/',
+    plugins: [react()],
+    define: {
+      'process.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL),
+      'process.env.VITE_PYTHON_API_URL': JSON.stringify(env.VITE_PYTHON_API_URL)
+    }
+  }
+})
