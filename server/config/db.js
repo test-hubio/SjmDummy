6c8c6d29a5c364696f3774779152b7b6e4effaa8
marvellous-is-cool <adebayoinioluwamarvellous1@gmail.com>
2025-01-13 15:10:03 +0100

    SJMINFIVERR - server config

--- server/config/db.js
+++ server/config/db.js
@@ -7,10 +7,10 @@ const connectDB = async () => {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
       waitForConnections: true,
       connectionLimit: 10,
       queueLimit: 0,
-      port: process.env.DB_PORT
     });
 
     // Test connection
