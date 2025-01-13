9b519615626f295949e40b01b90ae097461539df
marvellous-is-cool <adebayoinioluwamarvellous1@gmail.com>
2025-01-13 07:58:21 +0100

    SJMINFIVERR - client config to /app9

--- server/app.js
+++ server/app.js
@@ -39,7 +39,7 @@ const app = express();
 // CORS configuration
 app.use(
   cors({
-    origin: `${ENV.CLIENT_URL}:5173` || "http://localhost:5173",
+    origin: ENV.CLIENT_URL || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
